87c1c4a922011dc8d1b57726e44f461f44aff9b5
Paul Delafosse <paul.delafosse@protonmail.com>
2020-08-05 07:16:52 +0200

    docs: rewrite some fn docs

--- src/settings.rs
+++ src/settings.rs
@@ -7,7 +7,7 @@ use std::path::PathBuf;
 /// The Global bombadil configuration
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Settings {
-    /// User define dotfiles directory, usually your versionned dotfiles
+    /// User define dotfiles directory, usually your versioned dotfiles
     pub(crate) dotfiles_dir: PathBuf,
     /// A list of symlink to edit
     pub(crate) dot: Vec<DotLink>,
@@ -24,7 +24,7 @@ pub struct Var {
 
 impl Settings {
     /// Resolve bombadil settings against its standard xdg path :
-    /// `$XDG_CONFIG_DIR/bombadil/config.example.toml
+    /// `$XDG_CONFIG_DIR/bombadil.toml`
     pub fn get() -> Result<Self> {
         match Self::bombadil_config_xdg_path() {
             Ok(path) => {
