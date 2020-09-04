d5fc0e501f2509a2dd8e6d222601a85e4cb2d078
Paul Delafosse <paul.delafosse@protonmail.com>
2020-09-04 19:50:43 +0200

    fix(hook): add double quoted arg parsing

--- src/hook.rs
+++ src/hook.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use colored::*;
-use std::process::Command;
+use std::process::{Command, Stdio};
+use std::io::{BufReader, BufRead};
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct Hook {
@@ -11,7 +12,29 @@ impl Hook {
     pub(crate) fn run(&self) -> Result<()> {
         let command_display = format!("`{}`", &self.command.green());
         println!("Running post install hook : {}", command_display);
-        let args: Vec<&str> = self.command.split(' ').collect();
+
+        let args = self.split_args()?;
+        let mut command = Hook::build_command(args);
+
+        let mut child = command
+            .stderr(Stdio::piped())
+            .stdout(Stdio::piped())
+            .spawn()?;
+
+        BufReader::new(child.stdout.take().unwrap())
+            .lines()
+            .for_each(|line| println!("{}", line.unwrap_or_else(|_| "".into())));
+
+        BufReader::new(child.stderr.take().unwrap())
+            .lines()
+            .for_each(|line| eprintln!("{}", line.unwrap_or_else(|_| "".into())));
+
+        child.wait()
+            .map(|_| ())
+            .map_err(|err| anyhow!(err))
+    }
+
+    fn build_command(args: Vec<&str>) -> Command {
         let mut command = Command::new(args[0]);
         let mut pos = 1;
 
@@ -20,7 +43,39 @@ impl Hook {
             pos += 1;
         }
 
-        command.output().map_err(|err| anyhow!(err)).map(|_| ())
+        command
+    }
+
+    // TODO : generify for "'" and "`"
+    fn split_args(&self) -> Result<Vec<&str>> {
+        let mut indices: Vec<usize> = self.command.rmatch_indices("\"")
+            .map(|(idx, _)| idx)
+            .collect();
+
+        if indices.is_empty() {
+            return Ok(self.command.split(' ').collect());
+        }
+
+        if indices.len() % 2 != 0 {
+            return Err(anyhow!("Missing matching \\\""));
+        }
+
+        let mut args = vec![];
+        let mut cursor = 0;
+        while let (Some(start_quote_idx), Some(end_quote_idx)) = (indices.pop(), indices.pop()) {
+            let start = start_quote_idx + 1;
+            let split_space: Vec<&str> = self.command[cursor..start]
+                .split(' ')
+                .filter(|split| !split.is_empty())
+                .filter(|split| *split != "\"")
+                .collect();
+
+            args.extend_from_slice(&split_space);
+            args.push(&self.command[start..end_quote_idx]);
+            cursor = end_quote_idx + 1;
+        }
+
+        Ok(args)
     }
     pub fn new(command: &str) -> Self {
         let command = command.to_owned();
@@ -58,5 +113,47 @@ mod tests {
 
         // Assert
         assert!(result.is_err());
+    }
+
+    #[test]
+    fn should_split_args_with_quotes() {
+        // Arrange
+        let hook = Hook {
+            command: "echo \"hello Tom\"".to_string(),
+        };
+
+        // Act
+        let result = hook.split_args();
+
+        // Assert
+        assert_eq!(result.unwrap(), vec!["echo", "hello Tom"]);
+    }
+
+    #[test]
+    fn should_split_args_with_multiple_quotes() {
+        // Arrange
+        let hook = Hook {
+            command: "echo \"hello Tom\" || grep \"Toml\"".to_string(),
+        };
+
+        // Act
+        let result = hook.split_args();
+
+        // Assert
+        assert_eq!(result.unwrap(), vec!["echo", "hello Tom", "||", "grep", "Toml"]);
+    }
+
+    #[test]
+    fn should_split_args() {
+        // Arrange
+        let hook = Hook {
+            command: "git commit -m init".to_string(),
+        };
+
+        // Act
+        let result = hook.split_args();
+
+        // Assert
+        assert_eq!(result.unwrap(), vec!["git", "commit", "-m", "init"]);
     }
 }
