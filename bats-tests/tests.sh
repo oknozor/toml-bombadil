8035c61ef5e2dfeab77995325fae3f55478cf0d5
Paul Delafosse <paul.delafosse@protonmail.com>
2021-05-15 15:15:42 +0200

    chore: fix bats-file submodule remote

--- bats-tests/tests.sh
+++ bats-tests/tests.sh
@@ -17,10 +17,21 @@ load 'bats-file/load'
 @test "Symlink bombadil config" {
   run bombadil install -c "$HOME/dotfiles/bombadil.toml"
   assert_success
+  assert_symlink_to "$HOME/dotfiles/bombadil.toml" "$HOME/.config/bombadil.toml"
 }
 
 @test "Links dots" {
   run bombadil link
   assert_success
-  assert_file_exist "$HOME/.config/dummy.dot"
+  assert_file_exist "$HOME/dotfiles/.dots/dummy.dot"
+  assert_symlink_to "$HOME/dotfiles/.dots/dummy.dot" "$HOME/.config/dummy.dot"
 }
+
+@test "Vars injected" {
+  run bombadil link
+  assert_success
+  assert_file_exist "$HOME/.config/dummy.dot"
+  assert_file_contains "$HOME/.config/dummy.dot" "red is #de1f1f"
+  assert_file_contains "$HOME/.config/dummy.dot" "green is #41de1f"
+}
+
